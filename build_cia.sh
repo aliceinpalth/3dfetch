9a570e64e84c04822e2659f0cd04bbc32a1cab7f
Alice <alice.lfoster@alumni.ubc.ca>
2017-06-20 14:55:14 -0700

    Updated buildscript

--- build_cia.sh
+++ build_cia.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-makerom -f cia -target t -desc app:4 -rsf 3dfetch.rs -o 3dfetch.cia -exefslogo -icon icon.png -banner banner.png -elf 3dfetch.elf
-echo "cia built"
+3dstool -cvtf romfs romfs.bin --romfs-dir romfs/
+echo "romfs.bin recompiled"
+bannertool makebanner -i banner.png -a jingle.wav -o banner.bnr
+echo "Created banner"
+bannertool makesmdh -s "3dfetch" -l "3dfetch" -p "yyualice" -i icon.png -o icon.icn
+echo "Created icon"
+makerom -f cia -o 3dfetch.cia -DAPP_ENCRYPTED=false -rsf = 3dfetch.rsf -target t -exefslogo -elf 3dfetch.elf -icon icon.icn -banner banner.bnr -romfs romfs.bin
+echo "CIA built"
+echo "Done!"
