b2beebde99dc99845f78c29d4a61028572488887
Dawid 'daedreth' Eckert <eckertdawid@gmail.com>
2017-06-28 14:45:25 +0200

    rewrite

--- romfs/index.lua
+++ romfs/index.lua
@@ -1,154 +1,178 @@
 -- Some colors
-bgcolor = Color.new(0, 0, 0)
-rcolor = Color.new(255, 255, 255)
-lcolor = Color.new(0,255,255)
-lcolorIndex = 0
-rcolorIndex = 0
-bgcolorIndex = 0
+colors =
+{
+	background = Color.new(0, 0, 0),
+	right = Color.new(255, 255, 255),
+	left = Color.new(0, 255, 255)
+}
+
+-- Indexes
+colorIndex =
+{
+	background = 0,
+	right = 0,
+	left = 0
+}
+
+-- Global variables
 photoNum = 0
-oldpad = 0
-lastfp = ""
-
-isMenuOpen = false
-
-bitmap = Screen.createImage(1, 1, bgcolor)
-Graphics.init()
-picture = Graphics.loadImage("romfs:/images/luma.png")
-picture_b = Graphics.loadImage("romfs:/images/luma.png") 
-picture_g = Graphics.loadImage("romfs:/images/luma_g.png") 
-picture_p = Graphics.loadImage("romfs:/images/luma_p.png") 
-picture_y = Graphics.loadImage("romfs:/images/luma_y.png") 
-
-
+oldPad = 0
+lastFP = 0
 timer = Timer.new()
 shouldRenderSS = false
+isMenuOpen = false
 
--- Some helpers
-function printInfo()
-	-- Writing left side of screen
-	xoffset = 10
-	Screen.debugPrint(xoffset,10, getUsernameString(),rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,25,"-----------",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,55,"Firmware:",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,70,"Resolution:",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,85,"Kernel:",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,100,"CPU:",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,115,"Battery:",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,130,"Free space:",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,160,"Region:",lcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,175,"Date:",lcolor,TOP_SCREEN)
+-- Load default graphics
+bitmap = Screen.createImage(1, 1, colors.background)
+Graphics.init()
 
-	-- Writing right side of screen
-	xoffset = 160
-	Screen.debugPrint(xoffset,55,getCFW(),rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,70,"800x240, 320x240",rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,85,getKernelVer(),rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,100,getCPUString(),rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,115,getBatteryStatus(),rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,130,getFreeSpaceString(),rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,160,getRegion(),rcolor,TOP_SCREEN)
-	Screen.debugPrint(xoffset,175,date_string,rcolor,TOP_SCREEN)
+-- Images
+logos = 
+{
+	default = Graphics.loadImage("romfs:/images/isabelle.png"),
+	blue = Graphics.loadImage("romfs:/images/isabelle.png"),
+	green = Graphics.loadImage("romfs:/images/isabelle.png"),
+	pink = Graphics.loadImage("romfs:/images/isabelle.png"),
+	yellow = Graphics.loadImage("romfs:/images/isabelle.png")
+}
+
+-- Text output to top screen
+function printTopLeftSide()
+	local xoffset = 10
+
+	Screen.debugPrint(xoffset, 10, string.lower(getUsernameString()) .. "@" .. string.lower(getCPUString(1)), colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 25, "-----------", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 55, "Firmware:", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 70, "Resolution:", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 85, "Kernel:", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 100, "CPU:", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 115, "Battery:", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 130, "Free space:", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 160, "Region:", colors.left, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 175, "Date:", colors.left, TOP_SCREEN)
 end
 
+function printTopRightSide()
+	local xoffset = 160
+
+	Screen.debugPrint(xoffset, 55, getCFWString(), colors.right, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 70, "800x240, 320x240", colors.right, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 85, getKernelVersionString(), colors.right, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 100, getCPUString(2), colors.right, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 115, getBatteryStatusString(), colors.right, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 130, getFreeSpaceString(), colors.right, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 160, getRegionString(), colors.right, TOP_SCREEN)
+	Screen.debugPrint(xoffset, 175, getDateString(), colors.right, TOP_SCREEN)
+end
+
+-- Next 3 functions take care of the current date
 function getMonthString(month)
-	monthArray = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"}
+	local monthArray = 
+	{
+		"January",
+		"February",
+		"March",
+		"April",
+		"May",
+		"June",
+		"July",
+		"August",
+		"September",
+		"October",
+		"November",
+		"December"
+	}
+
 	return monthArray[month]
 end
 
 function getDayString(day)
-	dayArray = {"Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"}
+	local dayArray =
+	{
+		"Monday",
+		"Tuesday",
+		"Wednesday",
+		"Thursday",
+		"Friday",
+		"Saturday",
+		"Sunday"
+	}
+
 	return dayArray[day]
 end
 
-function updateLcolors()
-	if lcolorIndex == 0 then
-		lcolor = Color.new(0,255,255) -- Pastel blue
-		picture = picture_b
-	elseif lcolorIndex == 1 then
-		lcolor = Color.new(255,244,53) -- Pastel yellow
-		picture = picture_y
-	elseif lcolorIndex == 2 then
-		lcolor = Color.new(96,255,117) -- Pastel green
-		picture = picture_g
-	elseif lcolorIndex == 3 then
-		lcolor = Color.new(255,170,242) -- Pastel pink
-		picture = picture_p
-	elseif lcolorIndex > 3 then
-		lcolor = Color.new(0,255,255) -- Pastel blue
-		lcolorIndex = 0
-		picture = picture_b
-	elseif lcolorIndex < 0 then
-		lcolor = Color.new(255,204,247) -- Pastel pink
-		lcolorIndex = 3
-		picture = picture_p
-	end
+function getDateString()
+	local dayValue, day, month, year = System.getDate()
+	return getDayString(dayValue) .. " " .. getMonthString(month) .. " " .. day .. ", " .. year
 end
 
-function updateRcolors()
-	if rcolorIndex == 0 then
-		rcolor = Color.new(255,255,255) -- White
-	elseif rcolorIndex == 1 then
-		rcolor = Color.new(255,244,53) -- Pastel yellow
-	elseif rcolorIndex == 2 then
-		rcolor = Color.new(96,255,117) -- Pastel green
-	elseif rcolorIndex == 3 then
-		rcolor = Color.new(61,61,61) -- Black
-	elseif rcolorIndex > 3 then
-		rcolor = Color.new(255,255,255) -- White
-		rcolorIndex = 0
-	elseif rcolorIndex < 0 then
-		rcolor = Color.new(61,61,61) -- Black
-		rcolorIndex = 3
-	end
-end
-
-function updateBgcolors()
-	if bgcolorIndex == 0 then
-		bgcolor = Color.new(0,0,0) -- Black
-	elseif bgcolorIndex == 1 then
-		bgcolor = Color.new(0,98,137) -- Dark blue
-	elseif bgcolorIndex == 2 then
-		bgcolor = Color.new(255,255,255) -- White
-	elseif bgcolorIndex == 3 then
-		bgcolor = Color.new(0,175,96) -- Dark green
-	elseif bgcolorIndex > 3 then
-		bgcolor = Color.new(0,0,0) -- Black
-		bgcolorIndex = 0
-	elseif bgcolorIndex < 0 then
-		bgcolor = Color.new(0,136,21) -- Dark green
-		bgcolorIndex = 3
-	end
-end
-
--- IP address
--- This function was only added for potential expandability
-function getConsoleIPAddress()
-	return Network.getIPAddress()
-end
-
--- 3DS model
-model = System.getModel()
-modelArray = {"3ds", "3dsXL", "New3ds", "2ds", "New3dsXL", "New2dsXL"}
-processorArray = {"ARM11 Duocore", "ARM11 Duocore", "ARM11 Quadcore", "ARM11 Duocore", "ARM11 Quadcore", "ARM11 Quadcore"}
-model_string = modelArray[model+1]
-processor = processorArray[model+1]
-
--- Username
 function getUsernameString()
-	local rstring = System.getUsername() .. "@" .. model_string
-	return string.lower(rstring)
+	return System.getUsername()
 end
 
--- Kernel
-function getKernelVer()
-	majork,minork,revisionk = System.getKernel() 
-	return majork .. "." .. minork .. "-" .. revisionk
+function getCPUString(value)
+	model = System.getModel()
+
+	local modelArray =
+	{
+		"3ds",
+		"3dsXL",
+		"New3DS",
+		"2DS",
+		"New3DSXL",
+		"New2DSXL"
+	}
+
+	local processorArray =
+	{
+		"ARM11 Duocore",
+		"ARM11 Duocore",
+		"ARM11 Quadcore",
+		"ARM11 Duocore",
+		"ARM11 Quadcore",
+		"ARM11 Quadcore",
+	}
+	
+	if value == 1 then 
+		return modelArray[model + 1]
+	elseif value == 2 then
+		return processorArray[model + 1] .. " @ " .. System.getCpuSpeed() .. "hz"
+	else
+		return modelArray[model + 1], processorArray[model + 1]
+	end
+end
+
+function getRegionString()
+	regionNumber = System.getRegion()
+
+	local regionArray =
+	{
+		"North America",
+		"Europe",
+		"Japan"
+	}
+
+	return regionArray[regionNumber]
+end
+
+function getKernelVersionString()
+	local majorK, minorK, revisionK = System.getKernel()
+	return majorK .. "." .. minorK .. "." .. revisionK
+end
+
+function getBatteryStatusString()
+	if System.isBatteryCharging then return "Charging @ " .. System.getBatteryPercentage() .. "%" else
+					 return "Discharging @ " .. System.getBatteryPercentage() .. "%" end
+end
+
+function getFreeSpaceString()
+	return (math.floor(System.getFreeSpace()/1000000000)) .. "GB"
 end
 
 -- Improved CFW Function
 -- The Luma Version checking was adapted from astronautlevel2 and his StarUpdater, all credit where it's due
 -- Thanks Al <3
-function getCFW()
+function getCFWString()
 	local majorL,minorL,revisionL = System.getLumaVersion()
 	local fileContent = ""
 
@@ -183,152 +207,141 @@ function getCFW()
 				end
 				offset = offset + 1
 			end
-
 			return "Luma3DS " .. verString
 		end
+	
+	elseif string.match(fileContent, "skeith") then	return "Skeith"
+	elseif string.match(fileContent, "corbenik") then return  "Corbenik"
+	elseif string.match(fileContent, "Rei") then return "ReiNAND"
+	elseif string.match(fileContent, "cakes") then return "Cakes"
 	end
 
-	if string.match(fileContent, "skeith") then
-		return "Skeith"
+
+	if System.doesDirectoryExist("/rxTools") or System.doesFileExist("/rxTools.dat") then return "rxTools"
+	elseif System.doesDirectoryExist("/rei") then return "ReiNAND"
+	elseif System.doesDirectoryExist("/aurei") then return "AuReiNAND"
+	elseif System.doesDirectoryExist("/luma") then return "Luma3DS"
+	elseif System.doesDirectoryExist("/cakes") then return "Cakes"
+	end
+end
+
+function updateColors()
+	if colorIndex.left == 0 then
+		colors.left = Color.new(0,255,255) -- Pastel blue
+		logos.default = logos.blue
+	elseif colorIndex.left == 1 then
+		colors.left = Color.new(255,244,53) -- Pastel yellow
+		logos.default = logos.yellow
+	elseif colorIndex.left == 2 then
+		colors.left = Color.new(96,255,117) -- Pastel green
+		logos.default = logos.green
+	elseif colorIndex.left == 3 then
+		colors.left = Color.new(255,170,242) -- Pastel pink
+		logos.default = logos.pink
+	elseif colorIndex.left > 3 then
+		colors.left = Color.new(0,255,255) -- Pastel blue
+		colorIndex.left = 0
+		logos.default = logos.blue
+	elseif colorIndex.left < 0 then
+		colors.left = Color.new(255,204,247) -- Pastel pink
+		colorIndex.left = 3
+		logos.default = logos.pink
 	end
 	
-	if string.match(fileContent, "corbenik") then
-		return "Corbenik"
+	if colorIndex.right == 0 then
+		colors.right = Color.new(255,255,255) -- White
+	elseif colorIndex.right == 1 then
+		colors.right = Color.new(255,244,53) -- Pastel yellow
+	elseif colorIndex.right == 2 then
+		colors.right = Color.new(96,255,117) -- Pastel green
+	elseif colorIndex.right == 3 then
+		colors.right = Color.new(61,61,61) -- Black
+	elseif colorIndex.right > 3 then
+		colors.right = Color.new(255,255,255) -- White
+		colorIndex.right = 0
+	elseif colorIndex.right < 0 then
+		colors.right = Color.new(61,61,61) -- Black
+		colorIndex.right = 3
 	end
-
-	if string.match(fileContent, "Rei") then
-		return "ReiNAND"
-	end
-
-	if string.match(fileContent, "cakes") then
-		return "Cakes"
-	end
-
-	if System.doesDirectoryExist("/rxTools") or System.doesFileExist("/rxTools.dat") then
-		return "rxTools"
-	end
-
-	if System.doesDirectoryExist("/rei") then
-		return "ReiNAND"
-	end
-
-	if System.doesDirectoryExist("/aureinand") then
-		return "AuReiNAND"
-	end
-
-	if System.doesDirectoryExist("/luma") then
-		return "Luma3DS"
-	end
-
-	if System.doesDirectoryExist("/cakes") then
-		return "Cakes"
-	end
-end
-
--- Firmware -> deprecated
-function getLumaVer()
-	local majorL,minorL,revisionL = System.getLumaVersion()
-	return "Luma v" .. majorL .. "." .. minorL
-end
-
--- Region
-function getRegion()
-	region = System.getRegion()
-	regionArray = {"North America", "Europe", "Japan"}
-	return regionArray[region]
-end
-
--- Battery status
-function getBatteryStatus()
-	if System.isBatteryCharging() then
-		return "Charging @ " .. System.getBatteryPercentage() .. "%"
-	else
-		return "Discharging @ " .. System.getBatteryPercentage() .. "%"
+	
+	if colorIndex.background == 0 then
+		colors.background = Color.new(0,0,0) -- Black
+	elseif colorIndex.background == 1 then
+		colors.background = Color.new(0,98,137) -- Dark blue
+	elseif colorIndex.backrgound == 2 then
+		colors.background = Color.new(255,255,255) -- White
+	elseif colorIndex.background == 3 then
+		colors.background = Color.new(0,175,96) -- Dark green
+	elseif colorIndex.background > 3 then
+		colors.background = Color.new(0,0,0) -- Black
+		colorIndex.background = 0
+	elseif colorIndex.background < 0 then
+		colors.background = Color.new(0,136,21) -- Dark green
+		colorIndex.background = 3
 	end
 end
-
--- Date
-day_value,day,month,year = System.getDate()
-day_value_string = getDayString(day_value)
-month_string = getMonthString(month)
-date_string = day_value_string .. " " .. month_string .. " " .. day .. ", " .. year
-
--- CPU Frequency
-function getCPUString()
-	return processor .. " @ " .. System.getCpuSpeed() .. "hz"
-end
-
--- Free space
-function getFreeSpaceString()
-	return (math.floor(System.getFreeSpace()/1000000000)) .. " GB"
-end
-
--- Menu
-function showMenu()
-	Graphics.initBlend(BOTTOM_SCREEN)
-	Graphics.fillRect(0,320,0,240, bgcolor)
-	Graphics.termBlend()
-	Screen.debugPrint(20,20,"Menu Placeholder",lcolor, BOTTOM_SCREEN)
-end
-
 
 function initCFWLogo()
-
-	local cfw = getCFW()
+	local cfw = getCFWString()
 
 	if string.match(cfw, "Luma") then
-		picture = Graphics.loadImage("romfs:/images/luma.png")
-		picture_b = Graphics.loadImage("romfs:/images/luma.png") 
-		picture_g = Graphics.loadImage("romfs:/images/luma_g.png") 
-		picture_p = Graphics.loadImage("romfs:/images/luma_p.png") 
-		picture_y = Graphics.loadImage("romfs:/images/luma_y.png") 
+		logos.default = Graphics.loadImage("romfs:/images/luma.png")
+		logos.blue = Graphics.loadImage("romfs:/images/luma.png") 
+		logos.green = Graphics.loadImage("romfs:/images/luma_g.png") 
+		logos.pink = Graphics.loadImage("romfs:/images/luma_p.png") 
+		logos.yellow = Graphics.loadImage("romfs:/images/luma_y.png") 
 	elseif string.match(cfw, "Rei") then -- thanks datcom
-		picture = Graphics.loadImage("romfs:/images/rei.png")
-		picture_b = Graphics.loadImage("romfs:/images/rei.png") 
-		picture_g = Graphics.loadImage("romfs:/images/rei_g.png") 
-		picture_p = Graphics.loadImage("romfs:/images/rei_p.png") 
-		picture_y = Graphics.loadImage("romfs:/images/rei_y.png") 
+		logos.default = Graphics.loadImage("romfs:/images/rei.png")
+		logos.blue = Graphics.loadImage("romfs:/images/rei.png") 
+		logos.green = Graphics.loadImage("romfs:/images/rei_g.png") 
+		logos.pink = Graphics.loadImage("romfs:/images/rei_p.png") 
+		logos.yellow = Graphics.loadImage("romfs:/images/rei_y.png") 
 	elseif string.match(cfw, "rxTools") then -- thanks Al
-		picture = Graphics.loadImage("romfs:/images/rx.png")
-		picture_b = Graphics.loadImage("romfs:/images/rx.png") 
-		picture_g = Graphics.loadImage("romfs:/images/rx_g.png")
-		picture_p = Graphics.loadImage("romfs:/images/rx_p.png") 
-		picture_y = Graphics.loadImage("romfs:/images/rx_y.png") 
+		logos.default = Graphics.loadImage("romfs:/images/rx.png")
+		logos.blue = Graphics.loadImage("romfs:/images/rx.png") 
+		logos.green = Graphics.loadImage("romfs:/images/rx_g.png")
+		logos.pink = Graphics.loadImage("romfs:/images/rx_p.png") 
+		logos.yellow = Graphics.loadImage("romfs:/images/rx_y.png") 
 	end
+
 end
 
 function drawCFWLogo()
 	Graphics.initBlend(BOTTOM_SCREEN)
-	Graphics.fillRect(0,320,0,240,bgcolor)
-	Graphics.drawImage(0,0,picture)
+	Graphics.fillRect(0,320,0,240,colors.background)
+	Graphics.drawImage(0,0,logos.default)
 	Graphics.termBlend()
 end
 
--- Last function call before main loop engages
+function showMenu()
+	Graphics.initBlend(BOTTOM_SCREEN)
+	Graphics.fillRect(0, 320, 0, 240, colors.background)
+	Graphics.termBlend()
+	Screen.debugPrint(20, 20, "Menu Placeholder", colors.left, BOTTOM_SCREEN)
+end
+
+-- Last function calls before main loop engages
 initCFWLogo()
 
--- Main Loop
+-- Main loop
 while true do
-
 	seconds = math.ceil(Timer.getTime(timer)/1000)
 
-	-- Updating screens
 	Screen.refresh()
 
 	if isMenuOpen then showMenu() else drawCFWLogo() end
-	
+
 	Graphics.initBlend(TOP_SCREEN)
-	Graphics.fillRect(0,800,0,240, bgcolor)	
+	Graphics.fillRect(0, 800, 0, 250, colors.background)
 	Graphics.termBlend()
 
 	pad = Controls.read()
-	
-	printInfo()
 
-	filepath = ""
-	-- Screenshot if A is pressed
+	printTopLeftSide()
+	printTopRightSide()
+
 	if Controls.check(pad, KEY_A) and not (Controls.check(oldpad,KEY_A)) then
-		day_value, day, month, year = System.getDate() -- all ints
+		day_value, day, month, year = System.getDate()
 		filepath = "/" .. "3dfetch_" .. day .. "_" .. month .. "_" .. year .. "_" .. photoNum
 		lastfp = "/" .. "3dfetch_" .. day .. "_" .. month .. "_" .. year .. "_" .. photoNum
 		photoNum = photoNum + 1
@@ -336,7 +349,7 @@ while true do
 		System.takeScreenshot(filepath .. ".jpg", false)
 		shouldRenderSS = true
 	end
-
+	
 	-- Menu open
 	if Controls.check(pad, KEY_SELECT) and not (Controls.check(oldpad, KEY_SELECT)) then
 		if isMenuOpen then isMenuOpen = false elseif
@@ -345,32 +358,32 @@ while true do
 
 	-- Cycling lcolors
 	if Controls.check(pad, KEY_DRIGHT) and not (Controls.check(oldpad,KEY_DRIGHT)) then
-		lcolorIndex = lcolorIndex + 1
-		updateLcolors()
+		colorIndex.left = colorIndex.left + 1
+		updateColors()
 	end
 	if Controls.check(pad, KEY_DLEFT) and not (Controls.check(oldpad,KEY_DLEFT)) then
-		lcolorIndex = lcolorIndex - 1
-		updateLcolors()
+		colorIndex.left = colorIndex.left - 1
+		updateColors()
 	end
 
 	-- Cycling rcolors
 	if Controls.check(pad, KEY_DUP) and not (Controls.check(oldpad,KEY_DUP)) then
-		rcolorIndex = rcolorIndex + 1
-		updateRcolors()
+		colorIndex.right = colorIndex.right + 1
+		updateColors()
 	end
 	if Controls.check(pad, KEY_DDOWN) and not (Controls.check(oldpad,KEY_DDOWN)) then
-		rcolorIndex = rcolorIndex - 1
-		updateRcolors()
+		colorIndex.right = colorIndex.right - 1
+		updateColors()
 	end
 
 	-- Cycling bgcolors
 	if Controls.check(pad, KEY_R) and not (Controls.check(oldpad,KEY_R)) then
-		bgcolorIndex = bgcolorIndex + 1
-		updateBgcolors()
+		colorIndex.background = colorIndex.background + 1
+		updateColors()
 	end
 	if Controls.check(pad, KEY_L) and not (Controls.check(oldpad,KEY_L)) then
-		bgcolorIndex = bgcolorIndex - 1
-		updateBgcolors()
+		colorIndex.background = colorIndex.background - 1
+		updateColors()
 	end
 
 	if seconds > 5 then
@@ -379,8 +392,8 @@ while true do
 	end
 
 	if shouldRenderSS == true then
-		Screen.debugPrint(10,10, "Screenshot saved:",rcolor,BOTTOM_SCREEN)
-		Screen.debugPrint(10,25, lastfp,rcolor,BOTTOM_SCREEN)
+		Screen.debugPrint(10, 10, "Screenshot saved:", colors.right, BOTTOM_SCREEN)
+		Screen.debugPrint(10, 25, lastfp, colors.right, BOTTOM_SCREEN)
 	end
 
 	-- Flipping screen
@@ -402,3 +415,30 @@ while true do
 		System.exit()
 	end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
